dcf4120e55e38ebcd76465c6d54ee5534e3e7013
Nick Nissen <nick.nissen@uptimedevelopment.dk>
2020-05-05 15:28:41 +0200

    Add detect filament type

--- utils.js
+++ utils.js
@@ -26,6 +26,26 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function detectType(text)  {
+  const regex = /(PETG|PLA|ABS|ASA|RESIN|FLEX|nylon|PC|PP\+|PBT\+|PVA)/gim
+  const types = ["PETG", "PLA", "ABS", "ASA", "RESIN", "FLEX", "nylon", "PC", "PP+", "PBT+", "PVA"];
+
+  const matches = str.match(regex);
+  if (!matches) {
+    return 'unknow';
+  }
+
+  const groupd = matches.reduce((total, value) => {
+      total[value] = (total[value] || 0) + 1;
+      return total;
+  }, {});
+
+  return Object.entries(groupd) // Convert to [["ABS", 1], ["PLA", 2]]
+    .sort((a, b) => b[1] - a[1])  // Sort by most common [["PLA", 2],["ABS", 1]]
+    .shift() //take first element ["PLA", 2]
+    .shift(); // get type "PLA"
+}
+
 module.exports = {
     random, sleep, useCache
 }
